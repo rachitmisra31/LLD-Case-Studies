a7cadefaf6c70763c059078b1a65c53355be3c22
Rachit Misra <rachitmisra31@gmail.com>
2026-02-04 23:53:40 +0530

    Folder Structure Shifted

---
+++ casestudies/tictactoe/exception/InvalidMoveException.java
@@ -0,0 +1,7 @@
+package casestudies.tictactoe.exception;
+
+public class InvalidMoveException extends RuntimeException{
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+}
