efeff704a6bd46cf578d7c8d1e79f5170df7d675
Rachit Misra <rachitmisra31@gmail.com>
2026-02-05 00:51:48 +0530

    Players class committed

---
+++ casestudies/tictactoe/dataclasses/Players.java
@@ -0,0 +1,36 @@
+package casestudies.tictactoe.dataclasses;
+
+import casestudies.tictactoe.enums.Symbol;
+import casestudies.tictactoe.exception.InvalidMoveException;
+
+/**
+ * This class will decide the name of the players
+ * and there respective symbol and also it will throw the
+ * illegalmoveexception for any invalid move.
+ */
+public class Players {
+
+    private final String name;
+    private final Symbol symbol;
+    public Players(String name, Symbol symbol) {
+
+        if(symbol == Symbol.EMPTY)
+            throw new InvalidMoveException("Player has to make a move with O or X");
+        this.name = name;
+        this.symbol = symbol;
+    }
+    public String getName() {
+        return name;
+    }
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Players{" +
+                "name='" + name + '\'' +
+                ", symbol=" + symbol +
+                '}';
+    }
+}
