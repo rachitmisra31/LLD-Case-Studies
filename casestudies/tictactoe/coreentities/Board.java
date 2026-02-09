fea934c11bb91dc9e3a45aa11277d62ee36dee0a
Rachit Misra <rachitmisra31@gmail.com>
2026-02-09 20:30:31 +0530

    Board implemented

--- casestudies/tictactoe/coreentities/Board.java
+++ casestudies/tictactoe/coreentities/Board.java
@@ -8,55 +8,72 @@ public class Board {
 
     private final int size;
     private Cell[][] grid;
-    
-    public Board(int size){
+
+    public Board(int size) {
         this.size = size;
         grid = new Cell[size][size];
         initializeBoard();
     }
 
     private void initializeBoard() {
-        for(int i= 0; i<size; i++){
-            for(int j= 0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 grid[i][j] = new Cell();
             }
         }
     }
 
-    private void placeSymbol(int row, int col, Symbol symbol){
-        validatePosition( row, col);
+    public void placeSymbol(int row, int col, Symbol symbol) {
+        validatePosition(row, col);
         grid[row][col].setSymbol(symbol);
     }
 
-    private boolean isCellEmpty(int row, int col){
+    public boolean isCellEmpty(int row, int col) {
         validatePosition(row, col);
         return grid[row][col].isEmpty();
     }
 
     private void validatePosition(int row, int col) {
 
-        if(row<0 || row>= size || col<0 || col>= size){
+        if (row < 0 || row >= size || col < 0 || col >= size) {
             throw new InvalidMoveException("This is not a valid cell");
         }
     }
 
-    public Cell getCell(int row, int col){
+    public Cell getCell(int row, int col) {
         validatePosition(row, col);
         return grid[row][col];
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isFull(){
-        for(int i = 0; i<size; i++){
-            for(int j = 0; j<size; j++){
-                if(grid[i][j].isEmpty()){
+    public boolean isFull() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j].isEmpty()) {
                     return false;
                 }
             }
         }
         return true;
     }
+
+
+    public void printBoard() {
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + grid[i][j].getSymbol().getDisplayChar() + " ");
+                if (j < size - 1) System.out.print("|");
+            }
+            System.out.println();
+            if (i < size - 1) {
+                System.out.println("-".repeat(size * 4 - 1));
+            }
+        }
+        System.out.println();
+    }
+
 }
