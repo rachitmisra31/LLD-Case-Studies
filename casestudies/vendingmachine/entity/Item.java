2e5d480c43ade5dfd758af775265f775cc2b43ae
Rachit Misra <rachitmisra31@gmail.com>
2026-02-14 20:19:17 +0530

    Implement Item class to represent products in vending machine
    The Item class includes attributes such as code, name, and price,
    facilitating product management and interaction within the vending machine system.

---
+++ casestudies/vendingmachine/entity/Item.java
@@ -0,0 +1,22 @@
+package casestudies.vendingmachine.entity;
+
+public class Item {
+
+    private final String name;
+    private final String code;
+    private final int price;
+
+    public Item(String name, String code, int price){
+        this.name = name;
+        this.code = code;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
