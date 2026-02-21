4ea857681ef18068a5c505e0882e6b14c8560234
Rachit Misra <rachitmisra31@gmail.com>
2026-02-21 23:12:58 +0530

    LLD of music streaming implemented.

---
+++ casestudies/musicstreamingservice/state/PausedState.java
@@ -0,0 +1,23 @@
+package casestudies.musicstreamingservice.state;
+
+import casestudies.musicstreamingservice.entities.Player;
+import casestudies.musicstreamingservice.enums.PlayerStatus;
+
+public class PausedState implements PlayerState {
+    @Override
+    public void play(Player player) {
+        System.out.println("Resuming playback.");
+        player.changeState(new PlayingState());
+        player.setStatus(PlayerStatus.PLAYING);
+    }
+
+    @Override
+    public void pause(Player player) { System.out.println("Already paused."); }
+
+    @Override
+    public void stop(Player player) {
+        System.out.println("Stopping playback from paused state.");
+        player.changeState(new StoppedState());
+        player.setStatus(PlayerStatus.STOPPED);
+    }
+}
