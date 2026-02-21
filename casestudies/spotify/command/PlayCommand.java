fae7802ae95f75f5741d98589cd8200e7a3c02f8
Rachit Misra <rachitmisra31@gmail.com>
2026-02-21 22:40:54 +0530

    command added for pressing the play button of the music application.

---
+++ casestudies/spotify/command/PlayCommand.java
@@ -0,0 +1,12 @@
+package casestudies.spotify.command;
+
+import musicstreamingservice.entities.Player;
+
+public class PlayCommand implements Command {
+    private final Player player;
+
+    public PlayCommand(Player player) { this.player = player; }
+
+    @Override
+    public void execute() { player.clickPlay(); }
+}
