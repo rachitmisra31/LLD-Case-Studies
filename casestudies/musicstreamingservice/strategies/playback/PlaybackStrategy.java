1a59779fb142376c8be0c72118b029b8cf0c37c0
Rachit Misra <rachitmisra31@gmail.com>
2026-02-21 23:13:17 +0530

    LLD of music streaming implemented.

---
+++ casestudies/musicstreamingservice/strategies/playback/PlaybackStrategy.java
@@ -0,0 +1,14 @@
+package casestudies.musicstreamingservice.strategies.playback;
+
+import casestudies.musicstreamingservice.entities.Player;
+import casestudies.musicstreamingservice.entities.Song;
+import casestudies.musicstreamingservice.enums.SubscriptionTier;
+
+public interface PlaybackStrategy {
+    void play(Song song, Player player);
+
+    // Simple Factory method to get the correct strategy
+    static PlaybackStrategy getStrategy(SubscriptionTier tier, int songsPlayed) {
+        return tier == SubscriptionTier.PREMIUM ? new PremiumPlaybackStrategy() : new FreePlaybackStrategy(songsPlayed);
+    }
+}
