8477a8de114960fb4a4cf9f611c35ff996e0e0b5
Rachit Misra <rachitmisra31@gmail.com>
2026-02-27 15:00:07 +0530

    The code implemented for onlinestockexchange

---
+++ casestudies/onlinestockbrokeragesystem/state/FilledState.java
@@ -0,0 +1,15 @@
+package casestudies.onlinestockbrokeragesystem.state;
+
+import casestudies.onlinestockbrokeragesystem.entities.Order;
+
+public class FilledState implements OrderState {
+    @Override
+    public void handle(Order order) {
+        System.out.println("Order is already filled.");
+    }
+
+    @Override
+    public void cancel(Order order) {
+        System.out.println("Cannot cancel a filled order.");
+    }
+}
