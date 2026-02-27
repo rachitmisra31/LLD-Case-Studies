910b3ef98a0352df10513d0a6269b7f82d2e5071
Rachit Misra <rachitmisra31@gmail.com>
2026-02-27 15:00:45 +0530

    The code implemented for onlinestockexchange

---
+++ casestudies/onlinestockbrokeragesystem/state/OpenState.java
@@ -0,0 +1,18 @@
+package casestudies.onlinestockbrokeragesystem.state;
+
+import casestudies.onlinestockbrokeragesystem.entities.Order;
+import casestudies.onlinestockbrokeragesystem.enums.OrderStatus;
+
+public class OpenState implements OrderState {
+    @Override
+    public void handle(Order order) {
+        System.out.println("Order is open and waiting for execution.");
+    }
+
+    @Override
+    public void cancel(Order order) {
+        order.setStatus(OrderStatus.CANCELLED);
+        order.setState(new CancelledState());
+        System.out.println("Order " + order.getOrderId() + " has been cancelled.");
+    }
+}
