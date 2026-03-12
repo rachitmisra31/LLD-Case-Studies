215caf33dca7dd07c54b5e72e643f985e7819f3f
Rachit Misra <rachitmisra31@gmail.com>
2026-03-12 18:44:45 +0530

    The code implemented for parking lot

--- casestudies/parkinglot/models/ParkingFloor.java
+++ casestudies/parkinglot/models/ParkingFloor.java
@@ -1,4 +1,33 @@
 package casestudies.parkinglot.models;
 
+import casestudies.parkinglot.emums.VehicleSize;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ParkingFloor {
+    private BigDecimal floorNum;
+    private List<ParkingSpot> parkingSpots;
+
+    public ParkingFloor(BigDecimal floorNum, List<ParkingSpot> parkingSpots) {
+        this.floorNum = floorNum;
+        this.parkingSpots = parkingSpots;
+    }
+
+    public BigDecimal getFloorNum() {
+        return floorNum;
+    }
+
+    public ParkingSpot findAvailableParkingSpots(VehicleSize vehicleSize) {
+        for (ParkingSpot spot : parkingSpots) {
+            if(spot.isAvailable() && vehicleSize.ordinal()<spot.getSize().ordinal()){
+                return spot;
+            }
+        }
+        return null;
+    }
+
+    public List<ParkingSpot> getParkingSpots() {
+        return parkingSpots;
+    }
 }
