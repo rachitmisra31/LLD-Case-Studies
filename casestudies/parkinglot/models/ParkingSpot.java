ef6daa54a2acb0e44ba10cb2be5fa68b27783c93
Rachit Misra <rachitmisra31@gmail.com>
2026-03-12 18:32:13 +0530

    The code implemented for parking lot

---
+++ casestudies/parkinglot/models/ParkingSpot.java
@@ -0,0 +1,49 @@
+package casestudies.parkinglot.models;
+
+import casestudies.parkinglot.exception.ParkingException;
+
+public class ParkingSpot {
+
+    private String spotId;
+    private Vehicle parkedVehicle;
+
+    public ParkingSpot(String spotId, Vehicle vehicle) {
+        this.spotId = spotId;
+        this.parkedVehicle = vehicle;
+    }
+
+    public Vehicle parkVehicle(Vehicle vehicle){
+        if(vehicle.getSize().ordinal()> parkedVehicle.getSize().ordinal()){
+            throw new ParkingException("Vehicle cannot be parked here");
+        }
+
+        this.parkedVehicle = vehicle;
+        return parkedVehicle;
+    }
+
+    public void unparkVehicle(Vehicle vehicle){
+        if(parkedVehicle == null){
+            throw new ParkingException("Vehicle already unparked");
+        }
+        parkedVehicle = null;
+    }
+
+    public boolean isAvailable(){
+        return parkedVehicle == null;
+    }
+    public String getSpotId() {
+        return spotId;
+    }
+
+    public void setSpotId(String spotId) {
+        this.spotId = spotId;
+    }
+
+    public Vehicle getVehicle() {
+        return parkedVehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.parkedVehicle = vehicle;
+    }
+}
